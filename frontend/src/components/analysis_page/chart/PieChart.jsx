4daf85efc74537f8d6e60fe1def65afbd180cd1b
minchenlee <110967458+minchenlee@users.noreply.github.com>
2023-05-21 22:46:12 +0800

    feat: update chart color

--- frontend/src/components/analysis_page/chart/PieChart.jsx
+++ frontend/src/components/analysis_page/chart/PieChart.jsx
@@ -36,7 +36,7 @@ function PieChart( {Context, dataDict, name} ) {
     );
   }
 
-  ChartJS.register(ArcElement, Tooltip, Legend, Colors);
+  ChartJS.register(ArcElement, Tooltip, Legend);
   ChartJS.defaults.font.size = 14;
 
   const options = {
@@ -67,6 +67,23 @@ function PieChart( {Context, dataDict, name} ) {
       {
         label: 'Count',
         data: Object.values(dataDict),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+          'rgba(255, 159, 64, 0.6)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
         // backgroundColor: 
         //   [
         //     '#A7E68C',
@@ -74,7 +91,6 @@ function PieChart( {Context, dataDict, name} ) {
         //     '#FEC3A6',
         //   ]
         // ,
-        borderWidth: 1,
       },
     ],
   };
